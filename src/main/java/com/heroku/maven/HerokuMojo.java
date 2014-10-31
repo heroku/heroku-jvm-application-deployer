a2e51e81ae09a88673bf3aef052171cbcad11d47
Joe Kutner <jpkutner@gmail.com>
2014-10-31 11:15:00 -0500

    initial update

--- src/main/java/com/heroku/maven/HerokuMojo.java
+++ src/main/java/com/heroku/maven/HerokuMojo.java
@@ -1,8 +1,11 @@
 package com.heroku.maven;
 
+import org.apache.maven.model.Build;
+import org.apache.maven.model.Model;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
 import sun.misc.BASE64Encoder;
 
 import java.io.*;
@@ -16,6 +19,19 @@ import java.util.Map;
  * @requiresDependencyResolution test
  */
 public class HerokuMojo extends AbstractMojo {
+  /**
+   * The maven project.
+   *
+   * @parameter expression="project"
+   * @readonly
+   */
+  private MavenProject project;
+
+  /**
+   * @parameter expression="project.build.directory/project.build.finalName"
+   * @readonly
+   */
+  private File outputPath;
 
   /**
    * The name of the Heroku app.
@@ -23,7 +39,7 @@ public class HerokuMojo extends AbstractMojo {
    * Command line -Dheroku.appName=...
    *
    * @required
-   * @parameter expression="${heroku.appName}"
+   * @parameter expression="heroku.appName"
    */
   protected String appName = null;
 
@@ -32,7 +48,7 @@ public class HerokuMojo extends AbstractMojo {
    * <br/>
    * Command line -Dheroku.jdkVersion=...
    *
-   * @parameter expression="${heroku.jdkVersion}"
+   * @parameter expression="heroku.jdkVersion"
    *            default-value="1.7"
    */
   protected String jdkVersion = null;
@@ -42,14 +58,14 @@ public class HerokuMojo extends AbstractMojo {
    * <br/>
    * Command line -Dheroku.jdkUrl=...
    *
-   * @parameter expression="${heroku.jdkUrl}"
+   * @parameter expression="heroku.jdkUrl"
    */
   protected String jdkUrl = null;
 
   /**
    * Configuration variables that will be set on the Heroku app.
    *
-   * @parameter expression="${heroku.configVars}"
+   * @parameter expression="heroku.configVars"
    */
   protected Map<String,String> configVars = null;
 
@@ -57,29 +73,79 @@ public class HerokuMojo extends AbstractMojo {
    * The process types used to run on Heroku (similar to Procfile).
    *
    * @required
-   * @parameter expression="${heroku.configVars}"
+   * @parameter expression="heroku.configVars"
    */
   protected Map<String,String> processTypes = null;
+
+  private String encodedApiKey = null;
 
   @Override
   public void execute() throws MojoExecutionException, MojoFailureException {
 
   }
 
-  private String getApiKey() throws IOException {
-    String apiKey = System.getenv("HEROKU_API_KEY");
-    if (null == apiKey || apiKey.equals("")) {
-      ProcessBuilder pb = new ProcessBuilder().command("heroku", "auth:token");
-      Process p = pb.start();
+  private void deploy(File targetDir) {
 
-      BufferedReader bri = new BufferedReader(new InputStreamReader(p.getInputStream()));
-      String line;
-      apiKey = "";
-      while ((line = bri.readLine()) != null) {
-        apiKey += line;
+    getLog().info("---> Packaging application...");
+    getLog().info("     - app: " + appName);
+
+    // build the slug
+
+    // log included files
+
+    // copy included files to getAppDir()
+
+    // add slug extras
+
+    // createSlugData
+
+    // create Tar
+
+    // config var stuff...
+
+    // create slug
+
+    // upload slug
+
+    // release slug
+  }
+
+  private File getHerokuDir() {
+    return new File(getTargetDir(), "heroku");
+  }
+
+  private File getAppDir() {
+    return new File(getHerokuDir(), "app");
+  }
+
+  private File getAppTargetDir() {
+    return new File(getAppDir(), "target");
+  }
+
+  private File getTargetDir() {
+//    Model model = project.getModel();
+//    Build build = model.getBuild();
+//    return new File(build.getDirectory());
+    return outputPath;
+  }
+
+  private String getEncodedApiKey() throws IOException {
+    if (encodedApiKey == null) {
+      String apiKey = System.getenv("HEROKU_API_KEY");
+      if (null == apiKey || apiKey.equals("")) {
+        ProcessBuilder pb = new ProcessBuilder().command("heroku", "auth:token");
+        Process p = pb.start();
+
+        BufferedReader bri = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        String line;
+        apiKey = "";
+        while ((line = bri.readLine()) != null) {
+          apiKey += line;
+        }
       }
+      encodedApiKey = new BASE64Encoder().encode((":" + apiKey).getBytes());
     }
-    return new BASE64Encoder().encode((":" + apiKey).getBytes());
+    return encodedApiKey;
   }
 
   private String buildSlug(File targetDir, File appTargetDir, File herokuDir) {

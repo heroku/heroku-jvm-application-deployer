a5df3fdcc35843145748a41b2ce84a49ffdc5115
Joe Kutner <jpkutner@gmail.com>
2014-11-05 14:04:07 -0600

    improved config tests

--- src/it/deploy-config-test/verify.groovy
+++ src/it/deploy-config-test/verify.groovy
@@ -9,23 +9,23 @@ String appName = props["heroku.appName"]
 
 try {
     def log = FileUtils.fileRead(new File(basedir, "build.log"));
-    if (!log.contains("BUILD SUCCESS")) {
-        throw new RuntimeException("the build was not successful")
-    }
+    assert log.contains("BUILD SUCCESS"), "the build was not successful"
 
     def process = "heroku run cat test.txt -a${appName}".execute()
     process.waitFor()
     output = process.text
-    if (!output.contains("It worked!")) {
-        throw new RuntimeException("slug did not contain test file: ${output}")
-    }
+    assert output.contains("It worked!"), "slug did not contain test file: ${output}"
+
+    process = "heroku config -a${appName}".execute()
+    process.waitFor()
+    output = process.text
+    assert output.contains("MY_VAR"), "config var MY_VAR was not present"
+    assert output.contains("SomeValue"), "config var MY_VAR has the wrong value"
 
     process = "curl https://${appName}.herokuapp.com".execute()
     process.waitFor()
     output = process.text
-    if (!output.contains("Hello from Java!")) {
-        throw new RuntimeException("app is not running: ${output}")
-    }
+    assert output.contains("Hello from Java!"), "app is not running: ${output}"
 } finally {
    ("heroku destroy ${appName} --confirm ${appName}").execute().waitFor();
 }

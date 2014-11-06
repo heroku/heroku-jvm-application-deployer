905b1b4960e36950318b9e26aa9ae8bf87351b96
Joe Kutner <jpkutner@gmail.com>
2014-11-06 12:03:39 -0600

    added inclusion of pom.xml for war deployment.

--- maven-plugin/src/main/java/com/heroku/sdk/maven/MavenWarApp.java
+++ maven-plugin/src/main/java/com/heroku/sdk/maven/MavenWarApp.java
@@ -1,9 +1,11 @@
 package com.heroku.sdk.maven;
 
 import com.heroku.sdk.deploy.WarApp;
+import org.apache.commons.io.FileUtils;
 import org.apache.maven.plugin.logging.Log;
 
 import java.io.File;
+import java.util.List;
 
 public class MavenWarApp extends WarApp {
   private Log log;
@@ -11,6 +13,13 @@ public class MavenWarApp extends WarApp {
   public MavenWarApp(String name, File warFile, File webappRunnerJar, File rootDir, File targetDir, Log log) {
     super(name, warFile, webappRunnerJar, rootDir, targetDir);
     this.log = log;
+  }
+
+  @Override
+  public void prepare(List<File> includedFiles, String jdkVersion, String jdkUrl) throws Exception {
+    super.prepare(includedFiles, jdkVersion, jdkUrl);
+
+    FileUtils.copyFile(new File(getRootDir(), "pom.xml"), new File(getAppDir(), "pom.xml"));
   }
 
   public void logInfo(String message) {

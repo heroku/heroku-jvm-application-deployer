09bd48fe1a79a07a35bae8f6b7d37e523f13c7a4
Ansgar Brauner <ansgar.brauner@rewe-digital.com>
2015-01-07 15:08:49 +0100

    Removed MojoExecutor and added org.twdata.maven:mojo-executor:1.5.2 as a general dependency

--- maven-plugin/src/main/java/com/heroku/sdk/maven/executor/CopyDependencies.java
+++ maven-plugin/src/main/java/com/heroku/sdk/maven/executor/CopyDependencies.java
@@ -4,27 +4,28 @@ import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.BuildPluginManager;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
+import org.twdata.maven.mojoexecutor.MojoExecutor;
 
 public class CopyDependencies extends MojoExecutor {
 
-  public static void execute(MavenProject mavenProject,
-                             MavenSession mavenSession,
-                             BuildPluginManager pluginManager) throws MojoExecutionException {
-    executeMojo(
-        plugin(
-            groupId("org.apache.maven.plugins"),
-            artifactId("maven-dependency-plugin"),
-            version("2.4")
-        ),
-        goal("copy-dependencies"),
-        configuration(
-            element(name("outputDirectory"), "${project.build.directory}/dependency")
-        ),
-        executionEnvironment(
-            mavenProject,
-            mavenSession,
-            pluginManager
-        )
-    );
-  }
+    public static void execute(MavenProject mavenProject,
+                               MavenSession mavenSession,
+                               BuildPluginManager pluginManager) throws MojoExecutionException {
+        executeMojo(
+                plugin(
+                        groupId("org.apache.maven.plugins"),
+                        artifactId("maven-dependency-plugin"),
+                        version("2.4")
+                ),
+                goal("copy-dependencies"),
+                configuration(
+                        element(name("outputDirectory"), "${project.build.directory}/dependency")
+                ),
+                executionEnvironment(
+                        mavenProject,
+                        mavenSession,
+                        pluginManager
+                )
+        );
+    }
 }

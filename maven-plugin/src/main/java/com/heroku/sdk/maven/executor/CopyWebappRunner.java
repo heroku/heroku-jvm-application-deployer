2e2a2c86705b36c2b94ca3be943f74467b88d681
Joe Kutner <jpkutner@gmail.com>
2015-01-13 17:37:06 -0600

    Merge branch 'integrate-mojo-executor-as-dependecy' of https://github.com/abrauner/heroku-maven-plugin into abrauner-integrate-mojo-executor-as-dependecy
    Conflicts:
    	maven-plugin/src/main/java/com/heroku/sdk/maven/executor/CopyWebappRunner.java

--- maven-plugin/src/main/java/com/heroku/sdk/maven/executor/CopyWebappRunner.java
+++ maven-plugin/src/main/java/com/heroku/sdk/maven/executor/CopyWebappRunner.java
@@ -4,42 +4,45 @@ import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.BuildPluginManager;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
+import org.twdata.maven.mojoexecutor.MojoExecutor;
+
+import static org.twdata.maven.mojoexecutor.MojoExecutor.*;
 
 public class CopyWebappRunner extends MojoExecutor {
 
   public static final String DEFAULT_WEBAPP_RUNNER_VERSION = "7.0.57.1";
 
-  public static void execute(MavenProject mavenProject,
-                             MavenSession mavenSession,
-                             BuildPluginManager pluginManager) throws MojoExecutionException {
-    execute(mavenProject,mavenSession,pluginManager, DEFAULT_WEBAPP_RUNNER_VERSION);
-  }
+    public static void execute(MavenProject mavenProject,
+                               MavenSession mavenSession,
+                               BuildPluginManager pluginManager) throws MojoExecutionException {
+        execute(mavenProject, mavenSession, pluginManager, DEFAULT_WEBAPP_RUNNER_VERSION);
+    }
 
-  public static void execute(MavenProject mavenProject,
-                             MavenSession mavenSession,
-                             BuildPluginManager pluginManager,
-                             String webappRunnerversion) throws MojoExecutionException {
-    executeMojo(
-        plugin(
-            groupId("org.apache.maven.plugins"),
-            artifactId("maven-dependency-plugin"),
-            version("2.4")
-        ),
-        goal("copy"),
-        configuration(
-            element(name("artifactItems"),
-                element(name("artifactItem"),
-                    element(name("groupId"), "com.github.jsimone"),
-                    element(name("artifactId"), "webapp-runner"),
-                    element(name("version"), webappRunnerversion),
-                    element(name("destFileName"), "webapp-runner.jar")))
-        ),
-        executionEnvironment(
-            mavenProject,
-            mavenSession,
-            pluginManager
-        )
-    );
-  }
+    public static void execute(MavenProject mavenProject,
+                               MavenSession mavenSession,
+                               BuildPluginManager pluginManager,
+                               String webappRunnerversion) throws MojoExecutionException {
+        executeMojo(
+                plugin(
+                        groupId("org.apache.maven.plugins"),
+                        artifactId("maven-dependency-plugin"),
+                        version("2.4")
+                ),
+                goal("copy"),
+                configuration(
+                        element(name("artifactItems"),
+                                element(name("artifactItem"),
+                                        element(name("groupId"), "com.github.jsimone"),
+                                        element(name("artifactId"), "webapp-runner"),
+                                        element(name("version"), webappRunnerversion),
+                                        element(name("destFileName"), "webapp-runner.jar")))
+                ),
+                executionEnvironment(
+                        mavenProject,
+                        mavenSession,
+                        pluginManager
+                )
+        );
+    }
 }
 

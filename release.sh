8ba327be0e17ca45ed22b258c87d2b1b0bc89df9
Joe Kutner <jpkutner@gmail.com>
2015-01-30 14:30:24 -0600

    Update release script with articles to update [skip ci]

--- release.sh
+++ release.sh
@@ -1,1 +1,13 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+set -eu
+
 mvn release:prepare release:perform
+
+echo "Now make sure you update these articles and projects:
+
+    https://github.com/kissaten/maven-plugin-war-example
+    https://devcenter.heroku.com/articles/deploying-java-applications-with-the-heroku-maven-plugin
+    https://devcenter.heroku.com/articles/deploying-war-files-to-heroku-from-travis-ci
+"

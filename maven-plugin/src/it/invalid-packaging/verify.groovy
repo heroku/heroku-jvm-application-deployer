78956647883c5c8e146d9f0504e9df907eada0ce
Joe Kutner <jpkutner@gmail.com>
2015-04-03 08:23:55 -0500

    Ignore pom packaging if warFile config is defined. Fixes #15

--- maven-plugin/src/it/invalid-packaging/verify.groovy
+++ maven-plugin/src/it/invalid-packaging/verify.groovy
@@ -10,7 +10,7 @@ String appName = props["heroku.appName"]
 try {
     def log = FileUtils.fileRead(new File(basedir, "build.log"));
     assert log.contains("BUILD FAILURE"), "the build was successful but should NOT have been!"
-    assert log.contains("Your packaging must be set to 'war' to use this goal!"), "the build was successful but should NOT have been!"
+    assert log.contains("Your packaging must be set to 'war'"), "the build was successful but should NOT have been!"
 } finally {
    ("heroku destroy " + appName + " --confirm " + appName).execute().waitFor();
 }

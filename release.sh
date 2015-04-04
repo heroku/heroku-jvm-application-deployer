dae2cb56f1b31a9d2f700f9552dc20481ba0bf24
Joe Kutner <jpkutner@gmail.com>
2015-04-04 13:04:45 -0500

    Updated release script with a new link [skip ci]

--- release.sh
+++ release.sh
@@ -8,6 +8,7 @@ mvn release:prepare release:perform
 echo "Now make sure you update these articles and projects:
 
     https://github.com/kissaten/maven-plugin-war-example
+    https://github.com/jkutner/travis-heroku-java-example
     https://devcenter.heroku.com/articles/deploying-java-applications-with-the-heroku-maven-plugin
     https://devcenter.heroku.com/articles/deploying-war-files-to-heroku-from-travis-ci
 "

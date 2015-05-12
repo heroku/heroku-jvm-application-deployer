37b983ccf549c0a65f5dae7e78db8d667ed69251
Joe Kutner <jpkutner@gmail.com>
2015-05-12 14:12:31 -0500

    Added a new link to the release script

--- release.sh
+++ release.sh
@@ -7,6 +7,7 @@ mvn release:prepare release:perform
 
 echo "Now make sure you update these articles and projects:
 
+    https://github.com/kissaten/maven-plugin-example
     https://github.com/kissaten/maven-plugin-war-example
     https://github.com/jkutner/travis-heroku-java-example
     https://devcenter.heroku.com/articles/deploying-java-applications-with-the-heroku-maven-plugin

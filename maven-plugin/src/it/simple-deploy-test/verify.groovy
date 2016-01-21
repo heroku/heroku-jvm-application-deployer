a9260aef92682ed851f1db74d3abc03271696a4a
Joe Kutner <jpkutner@gmail.com>
2016-01-21 13:41:30 -0600

    Added sleep for one-off tests

--- maven-plugin/src/it/simple-deploy-test/verify.groovy
+++ maven-plugin/src/it/simple-deploy-test/verify.groovy
@@ -15,21 +15,25 @@ try {
     assert log.contains("heroku-maven-plugin app detected"), "jvm-common buildpack not detected"
     assert log.contains("BUILD SUCCESS"), "the build was not successful"
 
+    Thread.sleep(3000)
     def process = "heroku run cat target/mvn-dependency-list.log -a${appName}".execute()
     process.waitFor()
     output = process.text
     assert output.contains("The following files have been resolved"), "Dependencies not listed: ${output}"
 
+    Thread.sleep(3000)
     process = "heroku run java -version -a${appName}".execute()
     process.waitFor()
     output = process.text
     assert output.contains("1.8"), "Wrong version of JDK packaged into slug: ${output}"
 
+    Thread.sleep(3000)
     process = "heroku run with_jstack java -version -a${appName}".execute()
     process.waitFor()
     output = process.text
     assert output.contains("1.8"), "with_jstack failed: ${output}"
 
+    Thread.sleep(3000)
     process = "heroku run with_jmap java -version -a${appName}".execute()
     process.waitFor()
     output = process.text

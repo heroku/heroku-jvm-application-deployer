a053c4bcafefa9e5e929bc9fd9ef392a1aa2cbbd
Joe Kutner <jpkutner@gmail.com>
2016-04-27 13:35:09 -0500

    Upgrade the default webapp-runner version.

--- heroku-deploy/src/main/java/com/heroku/sdk/deploy/DeployWar.java
+++ heroku-deploy/src/main/java/com/heroku/sdk/deploy/DeployWar.java
@@ -9,7 +9,7 @@ import java.util.*;
 
 public class DeployWar extends WarApp {
 
-  public static final String DEFAULT_WEBAPP_RUNNER_VERSION = "8.0.23.0";
+  public static final String DEFAULT_WEBAPP_RUNNER_VERSION = "8.0.30.2";
 
   private static final String WEBAPP_RUNNER_URL_FORMAT="http://central.maven.org/maven2/com/github/jsimone/webapp-runner/%s/webapp-runner-%s.jar";
 

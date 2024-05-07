884984d6c360702d9dd78a4590fa358035ce3e10
Manuel Fuchs <manuel.fuchs@salesforce.com>
2024-05-07 10:54:53 +0200

    Fix NPE when Git repository is empty

--- integration-test/tests/integration_tests.rs
+++ integration-test/tests/integration_tests.rs
@@ -7,6 +7,8 @@ use std::process::Command;
 #[test]
 fn basic_war_app() {
     prepare_test("war-app", |context| {
+        create_empty_git_commit(&context.app_dir);
+
         run_command(
             Command::new("java")
                 .args([
@@ -32,6 +34,35 @@ fn basic_war_app() {
 #[test]
 fn basic_jar_app() {
     prepare_test("jar-app", |context| {
+        create_empty_git_commit(&context.app_dir);
+
+        run_command(
+            Command::new("java")
+                .args([
+                    "-jar",
+                    &heroku_jvm_application_deployer_jar_path().to_string_lossy(),
+                    &context
+                        .app_dir
+                        .join("target/test-1.0-SNAPSHOT-jar-with-dependencies.jar")
+                        .to_string_lossy(),
+                    "--jdk",
+                    "21",
+                ])
+                .current_dir(&context.app_dir),
+            "Running heroku-jvm-application-deployer failed.",
+            false,
+        );
+
+        let response = http_get_expect_200(&context.app_url);
+        assert_eq!(response, String::from("Hello World!"));
+    });
+}
+
+#[test]
+fn basic_jar_app_git_repo_without_commits() {
+    prepare_test("jar-app", |context| {
+        // Note that no git commit is created
+
         run_command(
             Command::new("java")
                 .args([
--- src/main/java/com/heroku/deployer/util/GitUtils.java
+++ src/main/java/com/heroku/deployer/util/GitUtils.java
@@ -2,19 +2,17 @@ package com.heroku.deployer.util;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.errors.RepositoryNotFoundException;
+import org.eclipse.jgit.lib.AnyObjectId;
 import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.ObjectId;
-
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Optional;
 
 public class GitUtils {
     public static Optional<String> getHeadCommitHash(Path projectDirectory) throws IOException {
-        try {
-            Git git = Git.open(projectDirectory.toFile());
-            ObjectId objectId = git.getRepository().resolve(Constants.HEAD);
-            return Optional.of(objectId.getName());
+        try (Git git = Git.open(projectDirectory.toFile())) {
+            return Optional.ofNullable(git.getRepository().resolve(Constants.HEAD))
+                    .map(AnyObjectId::getName);
 
         } catch (RepositoryNotFoundException e) {
             return Optional.empty();
